3d63f213a9b5bf27d192eb616dc2a7165f45e47a
Ben Parsons <ben+github@bpulse.co.uk>
2012-08-05 16:19:00 +0200

    custom data

--- data.js
+++ data.js
@@ -1,4 +1,17 @@
 var data = [];
+var qnumber = 1;
+var beatles = ["Golden Slumbers", "Sexy Sadie", "For No One", "Magical Mystery Tour", "Cry Baby Cry",
+  "Taxman", "Strawberry Fields Forever", "Paperback Writer", "Here Comes The Sun", "Carry That Weight"];
+
+for (item in beatles) {
+  data.push({track: beatles[item], artist:"The Beatles", number:qnumber, selector: "Nicky Wire", group:"The Beatles"});
+  qnumber++;
+}
+/*
+data.push({track: "Golden Slumbers", artist:"The Beatles", number:1, selector: "Nicky Wire", group:"The Beatles"})
+data.push({track: "Sexy Sadie", artist:"The Beatles", number:2, selector: "Nicky Wire", group:"The Beatles"})
+*/
+/*
 for (var i = 0; i < 500; i++) {
   data[i] = {
     title: "Task " + i,
@@ -9,3 +22,4 @@ for (var i = 0; i < 500; i++) {
     effortDriven: (i % 5 == 0)
   };
 }
+*/
